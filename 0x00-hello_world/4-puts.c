62cef916180724b0c5ab02ab8ef6cd1659f7be04
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-16 10:51:17 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include  <stdio.h>
-/*
- *main print a line of code using puts
+#include <stdio.h>
+/**
+ *main - print a line of code using puts
  *not allowed to use prinf
  *Return:0
  */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
