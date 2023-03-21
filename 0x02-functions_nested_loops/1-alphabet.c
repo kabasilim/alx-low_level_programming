9a5d29ef5bbbff2e753e0d779df7c2bcbeb22f62
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-21 11:46:38 -0700

    Task 1:  prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  * followed by newline
  */
 
-void print _alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a' ; letter <= 'z' ; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
 }
