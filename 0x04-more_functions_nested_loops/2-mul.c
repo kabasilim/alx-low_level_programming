a18c4fe298930d58b27cbeec7c1bf16bb5e44503
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-23 08:18:40 -0700

    Task 2: multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiples
+ * Return: product of the integers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
