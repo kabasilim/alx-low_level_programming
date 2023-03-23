44a4c23b6f308177a9c515c9ab782d3632a96f46
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-23 10:36:06 -0700

    Task 12: function that prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_number - Prints an integer.
+ *
+ * @n: The integer to be printed.
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + 0);
+}
