1974f90176fac89b64025b795b9120439c66c65c
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-23 12:31:51 -0700

    Task 12: function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,40 +1,35 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_number - Prints an integer.
- *
- * @n: The integer to be printed.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void print_number(int n)
 {
-	if (n >= 1024)
+	unsigned int m, d, count;
+
+	if (n < 0)
 	{
-	_putchar(n / 1000 + '0');
-	_putchar((n / 100) % 10 + '0');
-	_putchar((n / 10) % 10 + '0');
-	_putchar(n % 10 + '0');
-	}
-	else if (n >= 100)
-	{
-		_putchar((n / 10) / 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 10)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 0)
-	{
-		n = n * -1;
-		_putchar('-');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-		_putchar(n + '0');
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
 	}
 }
