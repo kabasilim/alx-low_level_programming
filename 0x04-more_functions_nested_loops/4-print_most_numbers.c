edfc9c9b3f9098353b39b7f64313e5b6b5ecab57
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-23 08:30:57 -0700

    Task 4: prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart
+ *                 from 2 and 4 and you can
+ *                 only use _putchar twice
+ * Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		if (i == 2 || i == 4)
+	{
+		_putchar(i + '0');
+	}
+	}
+	_putchar('\n');
+}
