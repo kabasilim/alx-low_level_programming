f816f5ec3903f0694f9fe9208c34233b25a42caf
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-03-31 14:06:32 -0700

    Task 11: Modified

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,78 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * infinite_add - adds two numbers
- * - both numbers would be strings
- * - the result should be stored in a buffer (string)
- * @n1: first number
- * @n2: second number
- * @r: a buffer where the result should be stored
- * @size_r: the size of the buffer
- *
- * Return: the buffer holding the result
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int size_n1 = 0, size_n2 = 0, op, bg, dr1, dr2, carryover = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (n1[size_n1])
-		size_n1++;
-	while (n2[size_n2])
-		size_n2++;
-	bg = size_n1 > size_n2 ? size_n1 : size_n2;
-	if (size_r <= bg + 1)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	r[bg + 1] = '\0';
-	size_n1--, size_n2--, size_r--;
-	dr1 = n1[size_n1] - 48;
-	dr2 = n2[size_n2] - 48;
-	while (bg >= 0)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		op = dr1 + dr2 + carryover;
-		carryover = (op >= 10) ? op / 10 : 0;
-		r[bg] = (op % 10) + 48;
-		if (size_n1 > 0)
-		{
-			size_n1--;
-			dr1 = n1[size_n1] - 48;
-		}
+		if (i < 0)
+			val1 = 0;
 		else
-			dr1 = 0;
-		if (size_n2 > 0)
-		{
-			size_n2--;
-			dr2 = n2[size_n2] - 48;
-		}
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
 		else
-			dr2 = 0;
-		bg--, size_r--;
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (*r == '0')
-		return (r + 1);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
