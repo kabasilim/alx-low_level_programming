0fbb45d704e8b466b11106861453dc0c5fbb0161
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-01 03:49:07 -0700

    Changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncpy - A function that copies a string.
- * @dest: pointer to destination input buffer
- * @src: pointer to source input buffer
- * @n: bytes of @src
- * Return: dest
+ * _strncpy - Function that copies a string.
+ * @dest: Pointer to destination input buffer.
+ * @src: Pointer to source input buffer.
+ * @n: Bytes of @src.
+ * Return: dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp - a function that compares two strings
- *
- * @s1: string 1 input to compare
- * @s2: against this other string 2
- * Return: difference between two strings
+ * _strcmp - Function that compares two strings.
+ * @s1: String input to compare.
+ * @s2: Against other string.
+ * Return: Difference between two strings.
  */
 
 int _strcmp(char *s1, char *s2)
