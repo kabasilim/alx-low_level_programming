f60a653e0da35a2899603f0af8b3fe47b769c706
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-01 04:17:13 -0700

    changes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
+ * rev_string - Reverse array.
+ * @n: integer parameter.
  * Return: 0
  */
 
 void rev_string(char *n)
 {
 	int i = 0;
-	int j = 0;
+	int a = 0;
 	char temp;
 
 	while (*(n + i) != '\0')
@@ -18,61 +18,62 @@ void rev_string(char *n)
 	}
 	i--;
 
-	for (j = 0; j < i; j++, i--)
+	for (a = 0; a < i; a++, i--)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
+		temp = *(n + a);
+		*(n + a) = *(n + i);
 		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
+ * infinite_add - Add two numbers together.
+ * @n1: Text representation of 1st number to add
+ * and
+ * @n2: Text representation of 2nd number to add.
+ * @r: Pointer to buffer.
+ * @size_r: Buffer size.
+ * Return: Pointer to calling function.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int inu = 0, i = 0, a = 0, numbers = 0;
+	int wor1 = 0, wor2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
-	while (*(n2 + j) != '\0')
-		j++;
+	while (*(n2 + a) != '\0')
+		a++;
 	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
+	a--;
+	if (a >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (a >= 0 || i >= 0 || inu == 1)
 	{
 		if (i < 0)
-			val1 = 0;
+			wor1 = 0;
 		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
+			wor1 = *(n1 + i) - '0';
+		if (a < 0)
+			wor2 = 0;
 		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
+			wor2 = *(n2 + j) - '0';
+		temp_tot = wor1 + wor2 + inu;
 		if (temp_tot >= 10)
-			overflow = 1;
+			inu = 1;
 		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
+			inu = 0;
+		if (numbers >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
+		*(r + numbers) = (temp_tot % 10) + '0';
+		numbers++;
+		a--;
 		i--;
 	}
-	if (digits == size_r)
+	if (numbers == size_r)
 		return (0);
-	*(r + digits) = '\0';
+	*(r + numbers) = '\0';
 	rev_string(r);
 	return (r);
 }
