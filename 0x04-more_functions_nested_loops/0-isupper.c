f7f81c2f3457334165ce81bcb4f75af4777de634
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-02 21:27:31 -0700

    Modified

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,12 @@
 /**
- * _isupper - check if c is upper
- *
- * @c: input for alphabet
- *
+ * _isupper - check if c is uppercase
+ * @c: input to check
  * Return: 1 if its uppercase, 0 if not
 */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= "A" && c <= "Z")
 		return (1);
 	else
 		return (0);
