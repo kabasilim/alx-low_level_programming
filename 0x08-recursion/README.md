274262354929753e93db3e98494cccd20a1bf90a
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-05 16:57:25 -0700

    Task 0:  function that prints a string, followed by a new line.

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+# 0x08-recursion
+
+## Description and script
+
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+int _putchar(char ch);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int square(int n, int v);
+int is_prime_number(int n);
+int is_prime(int n, int y);
+int is_palindrome(char *s);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair); 
+int wildcmp(char *s1, char *s2);
+
+#endif
