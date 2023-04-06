6e7cb3b44ea06e169101ff376e1eea10f42f0441
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-05 23:20:31 -0700

    correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,22 @@
 #include "main.h"
-
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number
  * Return: integer
- */
-
+*/
 int _sqrt_recursion(int n)
 {
-	return (sqrt_recursive(n, 1));
+	return (square(n, 1));
 }
 
 /**
- * sqrt_recursive - finds the square root of a number
+ * square - finds the square root of a number
  * @n: number
  * @v: value
  * Return: int
- */
+*/
 
-int sqrt_recursive(int n, int v)
+int square(int n, int v)
 {
 	if ((v * v) == n)
 	{
@@ -26,7 +24,7 @@ int sqrt_recursive(int n, int v)
 	}
 	else if ((v * v) < n)
 	{
-		return (sqrt_recursive(n, v + 1));
+		return (square(n, v + 1));
 	}
 	else
 	{
