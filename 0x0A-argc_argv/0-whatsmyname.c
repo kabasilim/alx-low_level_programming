30f854328fbbbee56480fa9814acb50d7343818a
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-10 09:15:39 -0700

    Task 0: Program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to a program that prints
+ * its name, followed by a new line.
+ * @argc: Number of things entered int the command line
+ * @argv: Array that hols the string value of argc
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
