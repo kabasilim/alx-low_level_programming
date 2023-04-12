f332ecf060c785a5a136d81a394be0312275b6b6
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-11 18:24:11 -0700

    Task 2: A function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: pointer to 2 dimensional array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				/*Free everything if malloc fails*/
+				while (i--)
+				{
+					free(tab[i]);
+				}
+				free(tab);
+				return (NULL);
+			}
+
+			for (j = 0; j < width; j++)
+			{
+				tab[i][j] = 0;
+			}
+		}
+	}
+	return (tab);
+}
