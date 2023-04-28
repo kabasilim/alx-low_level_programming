ab66b874331002fefc5bbe40b0bd81c410257521
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-27 18:58:37 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
  *
  * @head: head of linked list
  * @str: string to store list
- * Return: new_node
+ * Return: head
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -34,5 +34,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (head);
 }
