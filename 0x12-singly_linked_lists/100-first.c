e2d9028f6052350c7b94591b0bf8bff40be6cdf4
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-27 19:29:57 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
 
 void printMessage(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
 
