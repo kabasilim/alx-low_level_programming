c5b78a4625725336082141ea488541c17d2bcd80
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-04-27 19:25:10 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printMessage - function that runs before main
+ */
+
+void printMessage(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
+
