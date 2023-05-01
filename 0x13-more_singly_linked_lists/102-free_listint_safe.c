78c73127c653e931d1912c09ec657be8f473b7b1
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-05-01 14:26:03 -0700

    Completed

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,78 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define node listint_t
+
+/**
+ * find_loop - finds a loop in a list
+ * @head: the head of the list
+ * Return: the node where the loop begins, or NULL
+ *		if there is no loop
+ */
+
+node *find_loop2(node *head)
+{
+	node *slow, *fast;
+
+	if (!(head) || !(head->next))
+		return (NULL);
+	slow = head;
+	fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			fast = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - prints all nodes in a linked list
+ * @h: the head of the list
+ * Return: the number of nodes in the list
+ */
+
+size_t free_listint_safe(node **h)
+{
+	node *mark, *temp, *head;
+	size_t size, i;
+	char seen = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+	head = *h;
+	mark = find_loop2(head);
+	temp = head;
+
+	for (size = 0; temp != NULL; size++)
+	{
+		if (temp == mark)
+		{
+			if (seen)
+				break;
+			seen = 1;
+		}
+		temp = temp->next;
+	}
+	for (i = 0; i < size; i++)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	*h = NULL;
+	h = NULL;
+	return (size);
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#define node listint_t
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of the list
+ * Return: nothing!
+ */
+
+void free_listint2(node **head)
+{
+	if (head == NULL || *head == NULL)
+		return;
+	free_listint2(&((*head)->next));
+	free(*head);
+	*head = NULL;
+}
