790a66029f6ac21aade4093f99a128ce1ec499f1
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-08-21 20:09:06 -0700

    all

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: addition of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: subtraction of a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: numerator
+ * @b: denominator
+ * Return: division of a and b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the modulus of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: modulus of a and b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
